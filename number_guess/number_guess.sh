376073a97ccd7aa7101fa64dcd86a99b89f901a4
red9999 <47200164+red9999@users.noreply.github.com>
2022-12-14 10:59:59 +0900

    Update number_guess.sh

--- number_guess/number_guess.sh
+++ number_guess/number_guess.sh
@@ -18,7 +18,7 @@ MIN_GUESSES=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USERNAME_DATA
 echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $MIN_GUESSES guesses."
 fi
 # Ask for user to guess number
-echo "Guess the secret number between 1 and 1000: ($RANDOM_NUMBER)"
+echo "Guess the secret number between 1 and 1000:"
 read NUMBER_INPUT
 NUMBER_OF_TRIES=1
 while [ $NUMBER_INPUT != $RANDOM_NUMBER ]; do
@@ -26,18 +26,26 @@ while [ $NUMBER_INPUT != $RANDOM_NUMBER ]; do
   if ! [[ $NUMBER_INPUT =~ ^[0-9]+$ ]];then
   echo "$NUMBER_INPUT That is not an integer, guess again:"
     read NUMBER_INPUT
-  elif [[ $NUMBER_INPUT < $RANDOM_NUMBER ]]; then
-  # If lower
-  echo "It's lower than that, guess again: (tries:$NUMBER_OF_TRIES"
-  NUMBER_OF_TRIES=$NUMBER_OF_TRIES+1
-  read NUMBER_INPUT
   elif [[ $NUMBER_INPUT > $RANDOM_NUMBER ]]; then
+  # If lower
+  echo "It's lower than that, guess again:"
+  NUMBER_OF_TRIES=$(($NUMBER_OF_TRIES+1))
+  read NUMBER_INPUT
+  elif [[ $NUMBER_INPUT < $RANDOM_NUMBER ]]; then
   # If higher
   echo "It's higher than that, guess again:"
-  NUMBER_OF_TRIES=$($NUMBER_OF_TRIES + 1)
+  NUMBER_OF_TRIES=$(($NUMBER_OF_TRIES+1))
   read NUMBER_INPUT
   fi
 done
+if [[ $NUMBER_INPUT == $RANDOM_NUMBER ]]; then
+  if [[ -z $USERNAME_DATABASE ]]; then
+    INSERT_USERNAME_DATABASE=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME') ")
+    USERNAME_DATABASE=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+  fi
+  INSERT_GAMES=$($PSQL "INSERT INTO games(user_id,guesses) VALUES($USERNAME_DATABASE,$NUMBER_OF_TRIES) ")
+  echo "You guessed it in $NUMBER_OF_TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+fi
 
 
 # When number is guessed
